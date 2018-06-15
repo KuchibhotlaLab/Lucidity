887a2b3edb6fabbb74e14184f5fca339757079db
xli98 <theoria9898@hotmail.com>
2018-06-15 17:26:03 -0400

    Wireframe to dynamically link external devices; working on adding/displaying photo

--- app/src/main/java/com/mobile/lucidity/lucidity/AddWearableActivity.java
+++ app/src/main/java/com/mobile/lucidity/lucidity/AddWearableActivity.java
@@ -1,12 +1,23 @@
 package com.mobile.lucidity.lucidity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class AddWearableActivity extends AppCompatActivity {
 
@@ -16,21 +27,81 @@ public class AddWearableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_wearable);
 
 
-        final Button btn_add = findViewById(R.id.addWearable);
+        final FloatingActionButton btn_add = findViewById(R.id.addWearable);
+        ArrayList<String> buttons = new ArrayList<String>();
+
         btn_add.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Code here executes on main thread after user presses button
-                Intent intent = new Intent(getApplicationContext(), AddWearableFormActivity.class);
-                startActivity(intent);
+
+                LayoutInflater layoutInflater
+                        = (LayoutInflater)getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = layoutInflater.inflate(R.layout.device_dialog, null);
 
 
-                Button btn_wearable = new Button(getApplicationContext());
-                btn_wearable.setText("Push Me");
+                //TODO: edit text able to accept input
+                final EditText device = popupView.findViewById(R.id.device_to_add);
 
-                LinearLayout ll = findViewById(R.id.lnr_wearable);
-                ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                ll.addView(btn_wearable, lp);
-            }
-        });
+                /*final InputMethodManager imm = (InputMethodManager) getBaseContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                // Auto show keyboard
+                device.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                    @Override
+                    public void onFocusChange(View v, boolean isFocused) {
+
+                        if (isFocused)
+                        {
+                            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+                        }
+                    }
+                });*/
+
+
+                //inflate the add device dialog
+                final PopupWindow popupWindow = new PopupWindow(
+                        popupView,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+                popupWindow.setFocusable(true);
+
+                popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+
+
+
+
+                Button btnDismiss = (Button)popupView.findViewById(R.id.cancel_device);
+                btnDismiss.setOnClickListener(new Button.OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+                    }});
+
+                Button btnSave = (Button)popupView.findViewById(R.id.add_device);
+                btnSave.setOnClickListener(new Button.OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        // TODO: save the device information
+
+                        //TODO: get input string and put into cutomized button
+                        //TODO: set absolute position of button/put in tableview
+                        String deviceName = device.getText().toString();
+                        Toast.makeText(getBaseContext(), deviceName, Toast.LENGTH_LONG).show();
+
+                        //add new button associated with device
+                        Button btn_wearable = new Button(getApplicationContext());
+                        btn_wearable.setText(deviceName);
+
+                        LinearLayout ll = findViewById(R.id.lnr_wearable);
+                        ViewGroup.LayoutParams lp =
+                                new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                                        ViewGroup.LayoutParams.WRAP_CONTENT);
+                        ll.addView(btn_wearable, lp);
+
+
+                        popupWindow.dismiss();
+                    }
+                });
+            }});
     }
 }
--- app/src/main/java/com/mobile/lucidity/lucidity/PhotoActivity.java
+++ app/src/main/java/com/mobile/lucidity/lucidity/PhotoActivity.java
@@ -2,9 +2,17 @@ package com.mobile.lucidity.lucidity;
 
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -12,6 +20,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -21,21 +30,49 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class PhotoActivity extends AppCompatActivity {
+
+    private List<String> myList;
+    private GridView gridview;
+    private String mCurrentPhotoPath;
+    public static final String PREFS_NAME = "MyPrefsFile";
 
 
     private ArrayList<String> images;
 
+
+    ImageView targetImage;
+
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
 
-        GridView gallery = findViewById(R.id.photoGalary);
-        gallery.setAdapter(new ImageAdapter(this));
-        gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        FloatingActionButton loadImg = findViewById(R.id.loadimage);
+        targetImage = (ImageView)findViewById(R.id.targetimage);
+        gridview = findViewById(R.id.photoGalary);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        loadImg.setOnClickListener(new Button.OnClickListener(){
+
+        @Override
+        public void onClick(View arg0) {
+            // TODO Auto-generated method stub
+            Intent intent = new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, 0);
+        }});
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1,
@@ -50,7 +87,78 @@ public class PhotoActivity extends AppCompatActivity {
             }
         });
 
+
+
+
+
+
+
+        //TODO: code to take picture with camera
+        /*SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        myList = new ArrayList<String>();
+        myList.clear();
+        int size = prefs.getInt("myList.size()", 0);
+
+        for (int i=0; i < size; i++) {
+            String imagePath = prefs.getString(String.format("myList[%d]", i), "");
+        }
+
+        final Context con = this;
+        Button camera = findViewById(R.id.cameraButton);
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                dispatchTakePictureIntent();
+            }
+        });
+
+        gridview = (GridView) findViewById(R.id.photoGalary);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                // Send File Path to Image Display Activity
+                Intent intent = new Intent(getApplicationContext(), ImageDisplayActivity.class);
+                intent.putExtra("path", myList.get(position));
+                startActivity(intent);
+            }
+        });*/
+
+
     }
+
+
+    // To load single image
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+            Uri targetUri = data.getData();
+            Bitmap bitmap;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                targetImage.setImageBitmap(bitmap);
+
+                /*TODO: store this image in the string of images
+                 *refer to this further if fails:
+                 *https://stackoverflow.com/questions/28505123/getting-an-image-path-from-a-imageview*/
+
+                /*Uri path = Uri.parse("android.resource:com.mobile.lucidity.lucidity/" + targetImage.getId());
+                String imgPath = path.toString();
+                images.add(imgPath);*/
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     /**
      * The Class ImageAdapter.
@@ -96,8 +204,7 @@ public class PhotoActivity extends AppCompatActivity {
                 picturesView = (ImageView) convertView;
             }
 
-            Glide.with(context).load(images.get(position))
-                    .into(picturesView);
+            Glide.with(context).load(images.get(position)).into(picturesView);
 
             return picturesView;
         }
@@ -134,5 +241,119 @@ public class PhotoActivity extends AppCompatActivity {
             return listOfAllImages;
         }
     }
+
+
+    //taking a photo with app
+    /*static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photo = null;
+            try {
+                photo = createImageFile();
+            }
+            catch (IOException ex) {
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+
+            if(photo != null){
+                takePictureIntent.putExtra(MediaStore. EXTRA_OUTPUT,
+                        FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".my.package.name.provider", photo));
+
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+
+        } else {
+            Toast.makeText(this, "Another thing went wrong", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeS = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String picName = "JPEG_" + timeS + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile(picName, ".jpg", storageDir);
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+
+        return image;
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //ImageView mImageView = findViewById(R.id.image1);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Intent medIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE );
+            File f = new File(mCurrentPhotoPath );
+            Uri contentUri = Uri.fromFile(f);
+            medIntent.setData(contentUri);
+            this.sendBroadcast(medIntent);
+
+            myList.add(mCurrentPhotoPath);
+            gridview.invalidateViews();
+        }
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putInt("myList.size()", myList.size());
+
+        for (int i=0; i < myList.size(); i++) {
+            editor.putString(String.format("myList[%d]", i), myList.get(i));
+        }
+
+        editor.commit();
+
+
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context context) {
+            mContext = context;
+        }
+
+        public int getCount(){
+            return myList.size();
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+
+            if (convertView == null) {
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(8, 8, 8, 8);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inJustDecodeBounds = false ;
+            bmOptions.inSampleSize = 4;
+            bmOptions.inPurgeable = true ;
+            Bitmap bitmap = BitmapFactory.decodeFile(myList.get(position), bmOptions);
+
+            imageView.setImageBitmap(bitmap);
+
+            return imageView;
+        }
+    }*/
 
 }
